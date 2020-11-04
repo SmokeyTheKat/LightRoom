11dfa588c39d982c1ed8ade8dbfe4ec9eed3a786
= <owr5273@gmail.com>
2020-11-04 08:07:55 -0400

    idk

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ cvs.width  = 1000
 cvs.height = 600
 
 
+cvs.addEventListener("mousemove", function(e)
+{
+	dolight(e);
+});
+
+
+
 ctx.beginPath();
 ctx.rect(0, 0, cvs.width, cvs.height);
 ctx.fillStyle = "black";
@@ -14,7 +21,20 @@ ctx.strokeStyle = "white";
 //drawLine(0,0, 200, 200, "blue");
 
 
-light(0, 200, 460, 220, 2);
+function dolight(e)
+{
+	ctx.beginPath();
+	ctx.rect(0, 0, cvs.width, cvs.height);
+	ctx.fillStyle = "black";
+	ctx.fill();
+	
+	var area = cvs.getBoundingClientRect();
+	var x = Math.round((e.clientX - area.left))-1;
+	var y = Math.round((e.clientY - area.top))-1;
+	light(500,300, x, y, 13);
+}
+
+//light(0, 200, 160, 520, 5);
 //light(100,500,170,10, 5);
 
 function light(x, y, w, z, h)
@@ -26,8 +46,8 @@ function light(x, y, w, z, h)
 	var hitWalls = false;
 	var hitRoofs = false;
 
-	var htx = (0+b)/m;
-	var hbx = (600+b)/m;
+	var htx = Math.abs((0+b)/m);
+	var hbx = Math.abs((600+b)/m);
 	if (htx < 0 || htx > 1000 ||
 		hbx < 0 || hbx > 1000)// hit wall
 	{
@@ -38,7 +58,7 @@ function light(x, y, w, z, h)
 			var rm = -m;
 			var rb = ny-(rm*1000);
 			var ry = rm*0+rb;
-			light(1000, ny, ry, 0, h-1);
+			light(1000, ny, 0, ry, h-1);
 			return;
 		}
 		else
@@ -48,13 +68,33 @@ function light(x, y, w, z, h)
 			var rm = -(m);
 			var rb = ny-(rm*0);
 			var ry = rm*1000+rb;
-			light(0, ny, ry, 1000, h-1);
+			light(0, ny, 1000, ry, h-1);
 			return;
 			
 		}
 	}
-	if (htx < 0 || htx > 1000)// hit roof
+	else// hit roof
 	{
+		if (y < z)
+		{
+			var nx = (600-b)/m;
+			drawLine(x, y, nx, 600);
+			var rm = -m;
+			var rb = 600-(rm*nx);
+			var rx = (0-rb)/rm;
+			light(nx, 600, rx, 0, h-1);
+			return;
+		}
+		else
+		{
+			var nx = (0-b)/m;
+			drawLine(x, y, nx, 0);
+			var rm = -m;
+			var rb = 0-(rm*nx);
+			var rx = (600-rb)/rm;
+			light(nx, 0, rx, 600, h-1);
+			return;
+		}
 
 	}
 
